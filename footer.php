d3202f75c370cbce9ba999ec8fada519eb239c3a
Gilles Degottex <gilles.degottex@gmail.com>
2015-04-18 21:24:16 +0200

    first

---
+++ footer.php
@@ -0,0 +1,6 @@
+<!-- FOOTER  -->
+<div id="footer_wrap" class="outer">
+    <footer class="inner">
+    <p>For bug repports or idea suggestions, raise an <a target="_blank"  href="http://github.com/gillesdegottex/dfasma/issues">issue on GitHub</a> or send me an <a href="mailto:gilles.degottex@gmail.com?subject=dfasma">e-mail</a></p>
+    </footer>
+</div>
